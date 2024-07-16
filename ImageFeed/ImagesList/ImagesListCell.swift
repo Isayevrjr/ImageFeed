dc5c940e88184789d409978c3333813225f409c4
Rashid <rashid_isaev_1999@mail.ru>
2024-07-16 13:51:10 +0300

    Add Profile view controller

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -6,5 +6,24 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet var viewImage: UIImageView!
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var likeButton: UIButton!
+    @IBOutlet var gradientView: UIView!
     
+    override func awakeFromNib() {
+     super.awakeFromNib()
+     setUpGradientBackground()
+    }
+    
+    private func setUpGradientBackground() {
+        let gradient = CAGradientLayer()
+        gradient.frame = gradientView.bounds
+        gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientView.layer.insertSublayer(gradient, at: 0)
+        gradient.locations = [0.03, 2.8, 1]
+    }
 }
+   
+     
+    
+     
+
+  
