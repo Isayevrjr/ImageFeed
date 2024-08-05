6960d810ff27828ddf837c4b8c4b91d7e6c43a24
Rashid <rashid_isaev_1999@mail.ru>
2024-08-05 20:18:05 +0300

    Сетевой запрос/SplashScreen. 10 sprint done

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -1,7 +1,13 @@
 import UIKit
 
+protocol AuthViewControllerDelegate: AnyObject {
+    func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String)
+}
+
+
 final class AuthViewController: UIViewController {
     let showWebViewSegueIdentifier = "ShowWebView"
+    var delegate: AuthViewControllerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,7 +39,7 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
-        //
+        delegate?.authViewController(self, didAuthenticateWithCode: code)
     }
     
     func ViewViewControllerDidCancle(_ vc: WebViewViewController) {
---
+++ ImageFeed/URL + Extension/URLRequestExtension.swift
@@ -0,0 +1,16 @@
+//import UIKit
+//
+//fileprivate let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
+//
+//extension URLRequest {
+//    
+//    static func makeHTTPRequest(
+//        path: String,
+//        httpMethod: String,
+//        baseURL: URL = DefaultBaseURL
+//    ) -> URLRequest {
+//        var request = URLRequest(url: URL(string: path, relativeTo: baseURL) ?? DefaultBaseURL)
+//        request.httpMethod = httpMethod
+//        return request
+//    }
+//}
