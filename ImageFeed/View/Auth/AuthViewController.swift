b628e370fd35d68845a48a10b8b987dca9667b30
Rashid <rashid_isaev_1999@mail.ru>
2024-09-01 13:46:32 +0300

    Исправил!

--- ImageFeed/View/Auth/AuthViewController.swift
+++ ImageFeed/View/Auth/AuthViewController.swift
@@ -44,10 +44,9 @@ extension AuthViewController: WebViewViewControllerDelegate {
    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         dismiss(animated: true)
         UIBlockingProgressHUD.show()
-        oauth2Service.fetchAuthToken(code) { [weak self] result in
+        oauth2Service.fetchAuthToken(code) { [self] result in
            UIBlockingProgressHUD.dismiss()
-            guard let self = self else { return }
-            
+        
             switch result {
             case .success(let accessToken):
                 let tokenStorage = OAuth2TokenStorage()
