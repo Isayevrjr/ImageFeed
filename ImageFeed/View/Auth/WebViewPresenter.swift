475b3d60f76655a8ae2ace8ba50d8afb3f6a9215
Rashid <rashid_isaev_1999@mail.ru>
2024-09-21 00:50:33 +0300

    Конфигуратор + Хэлпер

--- ImageFeed/View/Auth/WebViewPresenter.swift
+++ ImageFeed/View/Auth/WebViewPresenter.swift
@@ -10,20 +10,14 @@ public protocol WebViewPresenterProtocol {
 final class WebViewPresenter: WebViewPresenterProtocol {
     
     weak var view: WebViewViewControllerProtocol?
+    var authHelper: AuthHelperProtocol
+    
+    init(authHelper: AuthHelperProtocol) {
+        self.authHelper = authHelper
+    }
     
     func viewDidLoad() {
-        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else
-        { return }
-        
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
-        ]
-        
-        guard let url = urlComponents.url else { return }
-        let request = URLRequest(url: url)
+        guard let request = authHelper.authRequest() else { return }
         didUpdateProgressValue(0)
         view?.load(request: request)
     }
@@ -41,15 +35,6 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     }
     
     func code(from url: URL) -> String? {
-        if let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "/oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" })
-        {
-            return codeItem.value
-        } else {
-            return nil
-        }
-        
+        authHelper.code(from: url)
     }
 }
