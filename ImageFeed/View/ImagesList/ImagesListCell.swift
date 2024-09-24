95569bc06a8f000ae26b4733d6625a8ff8ad16df
Rashid <rashid_isaev_1999@mail.ru>
2024-09-24 19:15:30 +0300

    ImageList - рефакторинг и тест

--- ImageFeed/View/ImagesList/ImagesListCell.swift
+++ ImageFeed/View/ImagesList/ImagesListCell.swift
@@ -14,20 +14,7 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet var gradientView: UIView!
     
     weak var delegate: ImagesListCellDelegate?
-    
-    override func awakeFromNib() {
-     super.awakeFromNib()
-     setUpGradientBackground()
-    }
-    
-    private func setUpGradientBackground() {
-        let gradient = CAGradientLayer()
-        gradient.frame = gradientView.bounds
-        gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
-        gradientView.layer.insertSublayer(gradient, at: 0)
-        gradient.locations = [0.03, 2.8, 1]
-    }
-    
+  
     override func prepareForReuse() {
         super.prepareForReuse()
         
@@ -39,8 +26,7 @@ final class ImagesListCell: UITableViewCell {
         likeButton.setImage(likeImage, for: .normal)
     }
     
-    
-    @IBAction func likeButtinClicked(_ sender: Any) {
+    @IBAction private func likeButtinClicked(_ sender: Any) {
         delegate?.imageListCellDidTapLike(self)
     }
     
